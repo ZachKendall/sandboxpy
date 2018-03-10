1b22c1e11a56526bc824f315c4e3b2a55d702785
ZachKendall <zacharyakendall@gmail.com>
2018-03-10 11:53:16 -0600

    added sandbox
    hello world

---
+++ sandbox.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+
+
+# basic copy-paste to start
+class SandboxApplication(tk.Frame):
+
+    def __init__(self, master=None):
+
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=root.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
+
+# Create app instance
+root = tk.Tk()
+app = SandboxApplication(master=root)
+
+# Start the app
+app.master.title("Sandbox")
+app.mainloop()
